a71d2f1315cf7843ad4e0856b739608e38216384
Jason Wong <jason.wongvh@gmail.com>
2021-02-04 18:06:17 +0800

    good

---
+++ network-graph.js
@@ -0,0 +1,17 @@
+sigma.parsers.json("data.json", {
+    container: "sigma-container",
+    settings: {
+        defaultNodeColor: "#000",
+        defaultEdgeColor: "#000",
+        defaultLabelColor: "#000",
+        edgeColor: "default",
+        enableCamera: false,
+        enableHovering: false,
+        mouseWheelEnabled: false,
+        font: "Raleway",
+        fontStyle: "bold",
+        defaultLabelSize: 20,
+        labelThreshold: 0,
+        maxNodeSize: 16,
+    },
+});
